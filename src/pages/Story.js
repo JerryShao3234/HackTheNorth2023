95d2ef13006bad592b0fc1c4590c7c69978e7ad0
Sophie Chai <chaisuefeisophie@gmail.com>
2023-09-16 11:20:14 -0400

    add story page

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -1,12 +1,20 @@
-
+import HTMLFlipBook from "react-pageflip";
+import * as React from 'react';
+import Box from '@mui/material/Box';
 
 export default function Story() {
+
     return (
         <div>
-            <h1>Prompt</h1>
-            <p>Prompt page</p>
-
-            {/* <button onClick={() => submitInput("Please explain why Jeff Bezos is rich")}>generate answer</button> */}
+        
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <HTMLFlipBook width={500} height={700}>
+                    <div className="demoPage">Page 1</div>
+                    <div className="demoPage">Page 2</div>
+                    <div className="demoPage">Page 3</div>
+                    <div className="demoPage">Page 4</div>
+                </HTMLFlipBook>
+            </Box>
 
         </div>
     )
