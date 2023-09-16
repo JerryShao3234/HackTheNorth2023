80aff2e674f9fd16df2e9ab869cf69d4accb2958
Peiyan Yang <iforgotpy@gmail.com>
2023-09-16 00:38:35 -0400

    generate API callback added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import './generate'
+import {submitInput} from './generate'
 
 function App() {
-  return (
-    <div className="App">
-      Our APP
-    </div>
-  );
+    return (
+        <div className="App">
+            Our APP
+            <button onClick={() => submitInput("Please explain why Jeff Bezos is rich")}>generate answer</button>
+        </div>
+    );
 }
 
 export default App;
--- src/generate.js
+++ src/generate.js
@@ -1,26 +1,32 @@
 import axios from 'axios'
 
-const options = {
-    method: 'POST',
-    url: 'https://api.cohere.ai/v1/generate',
-    headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        authorization: 'Bearer nMg4w9QRYmsi5cf9n8sA7HhjjPMsK432K4gcFB8I'
-    },
-    data: {
-        max_tokens: 20,
-        truncate: 'END',
-        return_likelihoods: 'NONE',
-        prompt: 'Please explain to me how LLMs work'
-    }
-};
+async function submitInput(text) {
+    const options = {
+        method: 'POST',
+        url: 'https://api.cohere.ai/v1/generate',
+        headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            authorization: 'Bearer nMg4w9QRYmsi5cf9n8sA7HhjjPMsK432K4gcFB8I'
+        },
+        data: {
+            max_tokens: 20,
+            truncate: 'END',
+            return_likelihoods: 'NONE',
+            prompt: text
+        }
+    };
 
-axios
-    .request(options)
-    .then(function (response) {
-        console.log(response.data);
-    })
-    .catch(function (error) {
-        console.error(error);
-    });
+    axios
+        .request(options)
+        .then(function (response) {
+            alert(response.data.generations[0].text);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+}
+
+export {submitInput}
+
+
